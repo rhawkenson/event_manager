b71ba68f4bdcd768abe2b237e8dfdefe82dbce70
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-10-14 09:36:22 -0500

    Add iteration 2: fix zipcodes psuedocode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,28 +1,45 @@
+# -----Iteration 0: Load file and access data-----
+
+# puts "EventManager Initialized!"
+
+# #Use the File.read class and method to access data 
+# contents = File.read "event_attendees.csv"
+# puts contents 
+
+# File.exist? "event_attendees.csv"
+
+
+# lines = File.readlines "event_attendees.csv"
+# lines.each do |line|
+#   next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end 
+
+# -----Iteration 1: Parsing with a CSV-----
+#Use the built-in Ruby CSV Parser 
+
+# require "csv"
+
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+# contents.each do |row|
+#   name = row[:first_name]
+#   zipcode = row[:zipcode]
+#   puts "#{name} #{zipcode}"
+# end 
+
+# -----Iteration 2: Cleaning up zipcodes -----
+# Steps to fix zipcode:
+#   If the zipcode is >5 digits, reduce the zipcode to the first 5 digits
+#   If the zip code is <5 digits, place a 0 at the beginning 
+#   If the zipcode is not present, set 00000 as the zipcode
+
 require "csv"
 
-puts "EventManager Initialized!"
-
-#Use the File.read class and method to access data 
-contents = File.read "event_attendees.csv"
-#puts contents 
-
-File.exist? "event_attendees.csv"
-
-
-lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-  next if line == " ,RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-  columns = line.split(",")
-  name = columns[2]
-  puts name
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end 
-
-#Use the built-in Ruby CSV Parser 
-  #See require statement line 1
-
-
-  # contents = CSV.open "event_attendees.csv"
-  # contents.each do |row|
-  #   name = row[2]
-  #   puts name
-  # end 
