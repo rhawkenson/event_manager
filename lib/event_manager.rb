b496fc8e3108ef6d76991cba8f1690ac19b80187
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-10-15 09:17:53 -0500

    Add form letter file for email thank you note to attendees

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -49,7 +49,44 @@
 #   puts "#{name} #{zipcode}"
 # end 
 
-# -----Iteration 3: Using Google's Civiv Information  -----
+# # -----Iteration 3: Using Google's Civiv Information  -----
+# require "csv"
+# require 'google/apis/civicinfo_v2'
+
+# civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+# civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+
+# def clean_zipcode(zipcode)
+#   zipcode.to_s.rjust(5, "0")[0..4] 
+# end 
+
+# def legislator_by_zipcode(zip)
+#   begin 
+#     legislators = civic_info.representative_info_by_address(
+#                               address: zipcode,
+#                               levels: 'country',
+#                               roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+#     legislators = legislators.officials
+    
+#     legislator_names = legislators.map(&:name) 
+#     legislator_string = legislator_names.join(", ")
+
+#   rescue
+#     "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+#   end
+# end
+
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+# contents.each do |row|
+#   name = row[:first_name]
+#   zipcode = clean_zipcode(row[:zipcode])
+#   legislators = legislator_by_zipcode(zipcode)
+#     puts "#{name} #{zipcode} #{legislators}"
+# end 
+
+# -----Iteration 4: Form Letters  -----
 require "csv"
 require 'google/apis/civicinfo_v2'
 
