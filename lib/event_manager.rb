cb13025f41a740c04502282324320bee00830431
Rebecca Hawkenson <rehawkenson@gmail.com>
2020-10-15 08:54:06 -0500

    Add method to get legislator by zipcode of each person in the data set

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -38,15 +38,7 @@
 # require "csv"
 
 # def clean_zipcode(zipcode)
-#   if zipcode.nil?
-#     "00000"
-#   elsif zipcode.length < 5
-#     zipcode.rjust(5, "0")
-#   elsif zipcode.length > 5
-#     zipcode.slice[0..4]
-#   else 
-#     zipcode
-#   end 
+#   zipcode.to_s.rjust(5, "0")[0..4] 
 # end 
 
 # contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
@@ -66,21 +58,30 @@ civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
 
 def clean_zipcode(zipcode)
-  if zipcode.nil?
-    "00000"
-  elsif zipcode.length < 5
-    zipcode.rjust(5, "0")
-  elsif zipcode.length > 5
-    zipcode.slice[0..4]
-  else 
-    zipcode
-  end 
+  zipcode.to_s.rjust(5, "0")[0..4] 
 end 
+
+def legislator_by_zipcode(zip)
+  begin 
+    legislators = civic_info.representative_info_by_address(
+                              address: zipcode,
+                              levels: 'country',
+                              roles: ['legislatorUpperBody', 'legislatorLowerBody'])
+    legislators = legislators.officials
+    
+    legislator_names = legislators.map(&:name) 
+    legislator_string = legislator_names.join(", ")
+
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
 contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
-  puts "#{name} #{zipcode}"
+  legislators = legislator_by_zipcode(zipcode)
+    puts "#{name} #{zipcode} #{legislators}"
 end 
